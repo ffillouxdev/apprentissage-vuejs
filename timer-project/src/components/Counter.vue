cfb0527a5dab526d43fddf6f441a68a66680d12c
FILLOUX Florian <florian.filloux@etu.univ-lyon1.fr>
2025-02-05 09:47:22 +0100

    feat(): projet timer et counter

---
+++ timer-project/src/components/Counter.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    import { computed, ref } from 'vue';
+
+    const counter = ref(0);
+    const joliC = computed(()=>{
+        const secondes = counter.value %60;
+        const minutes = Math.floor(counter.value / 60);
+        return `${minutes} minutes et ${secondes} secondes`;
+    });
+    setInterval(()=>{
+        counter.value++;
+    }, 1000);
+</script>
+
+<template>
+    <div class="counter">
+        <p>temps écoulé : {{ counter }} secondes.</p>
+        <p>cad {{ joliC }}</p>
+    </div>
+</template>
