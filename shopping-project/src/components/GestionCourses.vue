8b286a36c54292f0ffd978ed90f372fc0aa0a787
FILLOUX Florian <florian.filloux@etu.univ-lyon1.fr>
2025-02-10 09:17:13 +0100

    feat(): v-model

--- shopping-project/src/components/GestionCourses.vue
+++ shopping-project/src/components/GestionCourses.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
-const listec = [
+import type { CourseModel } from '@/models/CourseModel';
+import { ref } from 'vue';
+
+const listec : CourseModel[] = [
   {id:1, name:"navets"},
   {id:2, name:"kiwi"},
   {id:3, name:"radis"},  
 ]
+
+const nouveauNom = ref("");
+//if (nouveauNom.value !== "") listec.push({id:(listec.length+1), name: nouveauNom.value});
+const nouveauUrgent = ref(false);
+
 </script>
 <template>
     <div class="gestionnaire">
@@ -12,5 +20,16 @@ const listec = [
                 {{ item.name }}
             </li>
         </ul>
+        <h2>Ajouter un article:</h2>
+        <form action="">
+            <label>
+                Nouvel article:
+                <input v-model.lazy.trim="nouveauNom">
+            </label>
+            <label for="">
+                <input type="checkbox" v-model="nouveauUrgent" name="urgent" id="urgentCheckbox">
+            </label>
+        </form>
+        <p>Vous souhaitez ajouter {{ nouveauNom }} <strong v-if="nouveauUrgent">(urgent)</strong></p>
     </div>
 </template>
