9e769d5845811376466f02a4278edf1887fd0977
FILLOUX Florian <florian.filloux@etu.univ-lyon1.fr>
2025-02-10 08:52:22 +0100

    init(): shopping project

---
+++ shopping-project/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import GestionCourses from './components/GestionCourses.vue';
+</script>
+
+<template>
+  <main>
+    <h1>Ma liste de courses</h1>
+    <GestionCourses/>
+  </main>
+</template>
+
---
+++ shopping-project/src/components/GestionCourses.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const listec = [
+  {id:1, name:"navets"},
+  {id:2, name:"kiwi"},
+  {id:3, name:"radis"},  
+]
+</script>
+<template>
+    <div class="gestionnaire">
+        <ul>
+            <li v-for="item in listec" :key="item.id">
+                {{ item.name }}
+            </li>
+        </ul>
+    </div>
+</template>
