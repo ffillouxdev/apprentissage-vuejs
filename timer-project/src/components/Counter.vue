c9f2afaafaf6c87851f2debb625265b5b70c8e10
FILLOUX Florian <florian.filloux@etu.univ-lyon1.fr>
2025-02-10 09:49:41 +0100

    feat(): e.target

--- timer-project/src/components/Counter.vue
+++ timer-project/src/components/Counter.vue
@@ -17,16 +17,32 @@
     setTimeout(()=>{
         boutonDesactive.value =false;
     }, 6000);
+
+    function reset() : void {
+        counter.value = 0;
+    }
+
+    const texteBoutonClique = ref();
+    function listener(e : Event){
+        const element = e.target as HTMLElement;
+        if (element.tagName === "BUTTON"){
+            texteBoutonClique.value = element.textContent;
+        } 
+    }
 </script>
 
 <template>
-    <div class="counter">
+    <div class="counter" @click="listener">
         <p>temps écoulé : {{ counter }} secondes.</p>
         <p v-show="counter<=5">Bienvenue (v-show)</p>
         <p v-if="counter<=5">Bienvenue (v-if)</p>
         <p v-else-if="counter<=9">Le temps passe...</p>
         <p v-else>encore du temps passé...</p>
         <p>cad {{ joliC }}</p>
+        
+        <button :disabled="boutonDesactive" @click="reset">Recommencer</button>
+        <button :disabled="boutonDesactive">Test</button>
+        <p>Vous avez cliqué sur {{  texteBoutonClique }}</p>
     </div>
-    <button :disabled="boutonDesactive">Recommencer</button>
+    
 </template>
