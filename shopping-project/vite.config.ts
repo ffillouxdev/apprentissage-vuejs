81cf43d40aed85c779c7251561a6ad5795d348a3
FILLOUX Florian <florian.filloux@etu.univ-lyon1.fr>
2025-02-21 13:28:48 +0100

    feat(): base: dans vite.config.ts

--- shopping-project/vite.config.ts
+++ shopping-project/vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-beautiful-vue-app/',
   plugins: [
     vue(),
     vueDevTools(),
