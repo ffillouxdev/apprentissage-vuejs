bb15dbfe9276355b9a2b4ca3d526d3cda21f3893
FILLOUX Florian <florian.filloux@etu.univ-lyon1.fr>
2025-02-21 16:20:08 +0100

    fix(): changement de base  dans vite.config.ts 3

--- shopping-project/vite.config.ts
+++ shopping-project/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mes-courses/',
+  base: '/my-beautiful-vue-app/mes-courses/',
   plugins: [
     vue(),
     vueDevTools(),
